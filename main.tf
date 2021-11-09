25f422eb62a36dbb1d23e891b51795662f935fed
Elina <elina.ylkanen.gcp@gmail.com>
2021-11-09 10:39:46 +0200

    mainiin lisätty subnetwork ja vm-instanssi

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file) 
+  credentials = file(var.credentials_file)
 
   project = var.project
   region  = var.region
@@ -18,6 +18,42 @@ provider "google" {
 
 # VPC-networkin rakennus
 resource "google_compute_network" "vpc_network" {
-  name = "tuntikirjaus-vpc"
+  name                    = "tuntikirjaus-vpc"
   auto_create_subnetworks = false
+}
+
+#Subnetworkin lisääminen
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "tuntikirjaus-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+  
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
+resource "google_compute_network" "custom-test" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+
+
+# VM-instanssin luonti
+resource "google_compute_instance" "vm_instance" {
+  name         = "tuntikirjaus-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+   network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
